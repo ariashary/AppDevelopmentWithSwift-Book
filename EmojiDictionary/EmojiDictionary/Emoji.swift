21f56039eafa7bb3261e30402e73f81e260f942e
Muhammad Ashary <ariashary92@gmail.com>
2019-10-01 22:17:45 +0700

    Lab 4.7: Remember Emoji

--- EmojiDictionary/EmojiDictionary/Emoji.swift
+++ EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Emoji {
+struct Emoji: Codable {
     var symbol: String
     var name: String
     var description: String
@@ -19,5 +19,38 @@ struct Emoji {
         self.name = name
         self.description = description
         self.usage = usage
+    }
+    
+    static let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentDirectory.appendingPathComponent("emojis").appendingPathExtension("plist")
+    
+    static func saveToFile(emojis: [Emoji]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedNote = try? propertyListEncoder.encode(emojis)
+        try? encodedNote?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadFromFile() -> [Emoji]? {
+        let propertyListDecoder = PropertyListDecoder()
+        guard let retrieveEmojiData = try? Data(contentsOf: archiveURL) else {return nil}
+        let decodeNote = try? propertyListDecoder.decode(Array<Emoji>.self, from: retrieveEmojiData)
+        return decodeNote
+    }
+    
+    static func loadSampleEmojis() -> [Emoji] {
+        return [
+            Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+            Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+            Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
+            Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
+            Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
+            Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+            Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+            Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+            Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+            Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
+            Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
+            Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+            Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")]
     }
 }
--- EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -10,20 +10,7 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    var emojis: [Emoji] = [
-        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
-        Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-        Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
-        Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
-        Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
-        Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
-        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
-        Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-        Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
-        Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
-        Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-        Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")]
+    var emojis: [Emoji] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +19,12 @@ class EmojiTableViewController: UITableViewController {
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44.0
+        
+        if let savedEmojis = Emoji.loadFromFile() {
+            emojis = savedEmojis
+        } else {
+            emojis = Emoji.loadSampleEmojis()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,6 +63,7 @@ class EmojiTableViewController: UITableViewController {
         let movedEmoji = emojis.remove(at: sourceIndexPath.row)
         emojis.insert(movedEmoji, at: destinationIndexPath.row)
         tableView.reloadData()
+        Emoji.saveToFile(emojis: emojis)
     }
     
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
@@ -81,6 +75,7 @@ class EmojiTableViewController: UITableViewController {
             emojis.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
+        Emoji.saveToFile(emojis: emojis)
     }
     
     // MARK: - Navigation
@@ -108,6 +103,7 @@ class EmojiTableViewController: UITableViewController {
             emojis.append(emoji)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
+        Emoji.saveToFile(emojis: emojis)
     }
 
 }
