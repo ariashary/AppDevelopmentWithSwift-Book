53da9bd4eaeca96a895811fc3d63cecf3a823631
Muhammad Ashary <ariashary92@gmail.com>
2019-10-09 09:08:32 +0700

    Lesson 3.7: Tab Bar Controllers

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RedViewController.swift
+//  RainbowTabs
+//
+//  Created by Muhammad Ashary on 27/09/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
+    }
+
+
+}
+
